276786fc4d764dd5b1d7399ed07da01afac5a0e2
Adam Hansen <adamhansen@me.com>
2023-05-04 15:25:35 -0600

    Create getLength function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,36 @@
 // Query the DOM for the generate button
 var generateBtn = document.querySelector("#generate");
 
+// Prompt for or randomly generate the length of the password
+function getLength() {
+    // Prompt the user if they would like to specify the length of their password or use a random length
+    if ( confirm("Would you like to specify the length of your password or use a random length?") ) {
+        // Prompt the user for the length of their password
+        var length = prompt("How many characters would you like your password to be?");
+        // TODO: Cast the returned value as a number
+
+        // TODO: Validate the user's input
+
+        // Return the validated length
+        return length;
+    } else {
+        // TODO: Calculate a random length between 8 and 128
+
+        // TODO: Return a random length
+        return 8;
+    }
+}
+
 // Write password to the #password input
 function writePassword() {
     // Present user with a series of confirmation dialogs and and prompts for password criteria
-    var passwordLength = confirm("Would you like to specify the length of your password?");
-    var passwordLengthValue = prompt("How many characters would you like your password to be?");
+    var passwordLength = getLength;
     var passwordLowercase = confirm("Would you like to include lowercase letters?");
     var passwordUppercase = confirm("Would you like to include uppercase letters?");
     var passwordNumbers = confirm("Would you like to include numbers?");
     var passwordSpecial = confirm("Would you like to include special characters?");
 
-    // var password = generatePassword();    
+    // var password = generatePassword(passwordLength, passwordLowercase, passwordUppercase, passwordNumbers, passwordSpecial);    
     var passwordText = document.querySelector("#password");
 
     // passwordText.value = password;
